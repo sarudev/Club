7f8dc3e806631b2c90c6f3ac10b5ed6d1cc30be1
José María Coria <ExB8Ch@gmail.com>
2022-11-11 20:20:38 -0300

    clase menú terminada

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,68 +1,41 @@
 package menu;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Menu {
   private String title;
   private String description;
   private List<String> options;
   private int start;
-  private char type;
 
   public Menu(String title) {
     this.title = title;
     this.description = "";
-    this.start = 0;
-    this.type = '1';
+    this.start = 1;
+    this.options = new ArrayList<String>();
   }
 
   public Menu(String title, String description) {
     this.title = title;
     this.description = description;
-    this.start = 0;
-    this.type = '1';
+    this.start = 1;
+    this.options = new ArrayList<String>();
   }
 
-  public Menu(String title, String description, int start) {
+  public Menu(String title, String description, int start) throws Exception {
     this.title = title;
     this.description = description;
     this.start = start;
-    this.type = '1';
+    this.options = new ArrayList<String>();
   }
 
-  public Menu(String title, String description, char type) {
-    this.title = title;
-    this.description = description;
-    this.start = 0;
-    this.type = type;
-  }
-
-  public Menu(String title, String description, int start, char type) {
-    this.title = title;
-    this.description = description;
-    this.start = start;
-    this.type = type;
-  }
-
-  public Menu(String title, int start) {
+  public Menu(String title, int start) throws Exception {
     this.title = title;
     this.description = "";
     this.start = start;
-    this.type = '1';
-  }
-
-  public Menu(String title, char type) {
-    this.title = title;
-    this.description = "";
-    this.start = 0;
-    this.type = type;
-  }
-
-  public Menu(String title, int start, char type) {
-    this.title = title;
-    this.description = "";
-    this.start = start;
-    this.type = type;
+    this.options = new ArrayList<String>();
   }
 
   public String getTitle() {
@@ -85,7 +58,7 @@ public class Menu {
     return this.options;
   }
 
-  public void addOption(String option) {
+  public void addOption(String option) throws Exception {
     this.options.add(option);
   }
 
@@ -108,28 +81,44 @@ public class Menu {
     this.options.add(toIndex, option);
   }
 
-  public void print() {
+  public void print() throws Exception {
     for (int i = 0; i < 50; i++)
       System.out.println("");
     System.out.println(title);
     if (this.description.length() > 0)
-      System.out.println(description);
+      System.out.println("\n" + description);
 
-    char[] lowerCaser = {
-        'a', 'b', 'c', 'd', 'e', 'f',
-        'g', 'h', 'i', 'j', 'k', 'l',
-        'm', 'n', 'o', 'p', 'q', 'r',
-        's', 't', 'u', 'v', 'w', 'x',
-        'y', 'z'
-    };
+    System.out.println();
 
-    char[] upperCase = {
-        'A', 'B', 'C', 'D', 'E', 'F',
-        'G', 'H', 'I', 'J', 'K', 'L',
-        'M', 'N', 'O', 'P', 'Q', 'R',
-        'S', 'T', 'U', 'V', 'W', 'X',
-        'Y', 'Z'
-    };
+    for (int i = 0; i < this.options.size(); i++) {
+      String op = i + this.start + ". " + this.options.get(i);
+      System.out.println(op);
+    }
+  }
+
+  public int selectOption() throws Exception {
+    Scanner sc = new Scanner(System.in);
+    boolean ok = false;
+    int optionValue = 0;
+
+    while (!ok) {
+      System.out.print("Opcion? > ");
+      String option = sc.nextLine();
+      if (option.length() == 1) {
+        char cOption = option.charAt(0);
+        int value = 0;
+        if (Character.isDigit(cOption)) {
+          value = Integer.parseInt(Character.toString(cOption));
+        }
+        if (value >= this.start && value < this.options.size() + this.start) {
+          ok = true;
+          optionValue = value;
+        }
+      }
+    }
+
+    sc.close();
+    return optionValue;
   }
 
   private int getOptionIndex(String option) {
--- src/tests/Test.java
+++ src/tests/Test.java
@@ -7,8 +7,18 @@ public class Test {
 
 	public static void main(String[] args) {
 		Club club = new Club("Barrio feliz", 120, "Victor Hugo 1200");
-		Menu menu = new Menu("Bienvenido al club " + club.getNombre() + ".");
-		menu.print();
+		try {
+			Menu menu = new Menu("Bienvenido al club " + club.getNombre() + ".");
+			menu.addOption("Actividad 1");
+			menu.addOption("Actividad 2");
+			menu.addOption("Actividad 3");
+			menu.addOption("Actividad 4");
+			menu.print();
+			int option = menu.selectOption();
+			System.out.println(option);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
